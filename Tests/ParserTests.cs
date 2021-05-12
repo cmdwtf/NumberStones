ff300a325a5e2f4210f4292c818804ee55c3ef9f
Chris Dailey <nitz@users.noreply.github.com>
2021-05-12 14:17:30 -0400

    More tests.

--- Tests/ParserTests.cs
+++ Tests/ParserTests.cs
@@ -60,6 +60,8 @@ namespace cmdwtf.NumberStones.Tests
 		[DataRow("1d6x")]
 		[DataRow("1d6?")]
 		[DataRow("1d6cs>x")]
+		[DataRow("")]
+		[DataRow("foo")]
 		public void DiceParseThrowsOnPoorlyFormedDiceOptions(string input)
 		{
 			Assert.ThrowsException<Exceptions.DiceExpressionParseException>(() =>
@@ -71,18 +73,59 @@ namespace cmdwtf.NumberStones.Tests
 		public void DiceParseHandlesNoOptions() => ParseTest("1d20", "1d20");
 
 		[DataTestMethod]
+		[DataRow("8d6cf<5", "8d6cf<5")]
+		[DataRow("8d6cf<=5", "8d6cf<=5")]
+		public void DiceParseHandlesOptionCriticalFailure(string input, string expected)
+		{
+			ParseTest(input, expected);
+		}
+
+		[DataTestMethod]
+		[DataRow("8d6cs>5", "8d6cs>5")]
+		[DataRow("8d6cs>=5", "8d6cs>=5")]
+		public void DiceParseHandlesOptionCriticalSuccess(string input, string expected)
+		{
+			ParseTest(input, expected);
+		}
+
+		[DataTestMethod]
+		[DataRow("8d6d", "8d6dl1")]
+		[DataRow("8d6dl", "8d6dl1")]
+		[DataRow("8d6dl2", "8d6dl2")]
+		[DataRow("8d6dh", "8d6dh1")]
+		[DataRow("8d6dh2", "8d6dh2")]
+		public void DiceParseHandlesOptionDrop(string input, string expected)
+		{
+			ParseTest(input, expected);
+		}
+
+		[DataTestMethod]
+		[DataRow("8d6!", "8d6!")]
+		[DataRow("8d6!!", "8d6!!")]
+		[DataRow("8d6!p", "8d6!p")]
+		[DataRow("8d6!>4", "8d6!>4")]
+		[DataRow("8d6!!>4", "8d6!!>4")]
+		[DataRow("8d6!p>4", "8d6!p>4")]
+		public void DiceParseHandlesOptionExplode(string input, string expected)
+		{
+			ParseTest(input, expected);
+		}
+
+		[DataTestMethod]
 		[DataRow("8d6k", "8d6kh1")]
 		[DataRow("8d6kh", "8d6kh1")]
 		[DataRow("8d6kh2", "8d6kh2")]
+		[DataRow("8d6kl", "8d6kl1")]
+		[DataRow("8d6kl2", "8d6kl2")]
 		public void DiceParseHandlesOptionKeep(string input, string expected)
 		{
 			ParseTest(input, expected);
 		}
 
 		[DataTestMethod]
-		[DataRow("8d6t2", "8d6t2")]
-		[DataRow("8d6t2t4", "8d6t2t4")]
-		public void DiceParseHandlesOptionTwice(string input, string expected)
+		[DataRow("8d6[cha]", "8d6[cha]")]
+		[DataRow("8d6[charisma is the only stat that matters, but dex is the only important one]", "8d6[charisma is the only stat that matters, but dex is the only important one]")]
+		public void DiceParseHandlesOptionLabel(string input, string expected)
 		{
 			ParseTest(input, expected);
 		}
@@ -91,6 +134,21 @@ namespace cmdwtf.NumberStones.Tests
 		[DataRow("8d6r2", "8d6r2")]
 		[DataRow("8d6r2r4", "8d6r2r4")]
 		public void DiceParseHandlesOptionReroll(string input, string expected)
+		{
+			ParseTest(input, expected);
+		}
+
+		[DataTestMethod]
+		[DataRow("8d6", "8d6")]
+		public void DiceParseHandlesOptionTarget(string input, string expected)
+		{
+			ParseTest(input, expected);
+		}
+
+		[DataTestMethod]
+		[DataRow("8d6t2", "8d6t2")]
+		[DataRow("8d6t2t4", "8d6t2t4")]
+		public void DiceParseHandlesOptionTwice(string input, string expected)
 		{
 			ParseTest(input, expected);
 		}
@@ -108,6 +166,11 @@ namespace cmdwtf.NumberStones.Tests
 		[DataRow("8d20kh2dl1cs>=5cf<2t3", "8d20kh2dl1cs>=5cf<2t3")]
 		[DataRow("8d20kh2dl1cs>=5cf<2t3r4", "8d20kh2dl1cs>=5cf<2t3r4")]
 		[DataRow("8d20kh2dl1cs>=5cf<2t3r4=2", "8d20kh2dl1cs>=5cf<2t3r4=2")]
+		[DataRow("8d20kh2dl1cs>=5cf<2t3r4=2[neat]", "8d20kh2dl1cs>=5cf<2t3r4=2[neat]")]
+		[DataRow("8d20kh2dl1cs>=5cf<2t3r4=2[neat]!", "8d20kh2dl1cs>=5cf<2t3r4=2[neat]!")]
+		[DataRow("8d20kh2dl1cs>=5cf<2t3r4=2[neat]!>19", "8d20kh2dl1cs>=5cf<2t3r4=2[neat]!>19")]
+		[DataRow("8d20kh2dl1cs>=5cf<2t3r4=2[neat]!!>19", "8d20kh2dl1cs>=5cf<2t3r4=2[neat]!!>19")]
+		[DataRow("8d20kh2dl1cs>=5cf<2t3r4=2[neat]!p>19", "8d20kh2dl1cs>=5cf<2t3r4=2[neat]!p>19")]
 		public void DiceParseHandlesComplexOptionStrings(string input, string expected)
 		{
 			ParseTest(input, expected);
