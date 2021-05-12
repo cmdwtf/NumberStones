e3267d19f657b3fdce9a349cf0bf10d99d57fcf0
Chris Dailey <nitz@users.noreply.github.com>
2021-05-12 13:02:00 -0400

    More tests.

--- Tests/ParserTests.cs
+++ Tests/ParserTests.cs
@@ -25,6 +25,7 @@ namespace cmdwtf.NumberStones.Tests
 			Assert.IsNotNull(expression);
 			Assert.IsFalse(expression.IsEmpty);
 			string parsedExpression = expression.ToString();
+			Tools.Write(input, expression, null, expected);
 			Assert.AreEqual(expected, parsedExpression);
 		}
 
@@ -33,12 +34,94 @@ namespace cmdwtf.NumberStones.Tests
 		[DataRow("1d6 + (1 + 1")]
 		[DataRow("1d6 + 1 )")]
 		[DataRow("1d6 + 1[label")]
-		public void ParserFailsOnPoorlyFormedExpressions(string input)
+		public void DiceTryParseFailsOnPoorlyFormedExpressions(string input)
+		{
+			bool result = Dice.TryParse(input, out DiceExpression expression);
+			Assert.IsNotNull(expression);
+			Assert.IsFalse(result);
+			Assert.IsTrue(expression.IsEmpty);
+		}
+
+		[DataTestMethod]
+		[DataRow("1d6 *")]
+		[DataRow("1d6 + (1 + 1")]
+		[DataRow("1d6 + 1 )")]
+		[DataRow("1d6 + 1[label")]
+		public void DiceParseThrowsOnPoorlyFormedExpressions(string input)
+		{
+			Assert.ThrowsException<Exceptions.DiceExpressionParseException>(() =>
+				Dice.Parse(input)
+			);
+		}
+
+
+		[DataTestMethod]
+		[DataRow("1d6cx>14")]
+		[DataRow("1d6x")]
+		[DataRow("1d6?")]
+		[DataRow("1d6cs>x")]
+		public void DiceParseThrowsOnPoorlyFormedDiceOptions(string input)
+		{
+			Assert.ThrowsException<Exceptions.DiceExpressionParseException>(() =>
+				Dice.Parse(input)
+			);
+		}
+
+		[TestMethod]
+		public void DiceParseHandlesNoOptions() => ParseTest("1d20", "1d20");
+
+		[DataTestMethod]
+		[DataRow("8d6k", "8d6kh1")]
+		[DataRow("8d6kh", "8d6kh1")]
+		[DataRow("8d6kh2", "8d6kh2")]
+		public void DiceParseHandlesOptionKeep(string input, string expected)
+		{
+			ParseTest(input, expected);
+		}
+
+		[DataTestMethod]
+		[DataRow("8d6t2", "8d6t2")]
+		[DataRow("8d6t2t4", "8d6t2t4")]
+		public void DiceParseHandlesOptionTwice(string input, string expected)
+		{
+			ParseTest(input, expected);
+		}
+
+		[DataTestMethod]
+		[DataRow("8d6r2", "8d6r2")]
+		[DataRow("8d6r2r4", "8d6r2r4")]
+		public void DiceParseHandlesOptionReroll(string input, string expected)
+		{
+			ParseTest(input, expected);
+		}
+
+		[DataTestMethod]
+		[DataRow("8d20", "8d20")]
+		[DataRow("8d20k", "8d20kh1")]
+		[DataRow("8d20kh", "8d20kh1")]
+		[DataRow("8d20kh2", "8d20kh2")]
+		[DataRow("8d20kh2d", "8d20kh2dl1")]
+		[DataRow("8d20kh2dl", "8d20kh2dl1")]
+		[DataRow("8d20kh2dl1", "8d20kh2dl1")]
+		[DataRow("8d20kh2dl1cs>=5", "8d20kh2dl1cs>=5")]
+		[DataRow("8d20kh2dl1cs>=5cf<2", "8d20kh2dl1cs>=5cf<2")]
+		[DataRow("8d20kh2dl1cs>=5cf<2t3", "8d20kh2dl1cs>=5cf<2t3")]
+		[DataRow("8d20kh2dl1cs>=5cf<2t3r4", "8d20kh2dl1cs>=5cf<2t3r4")]
+		[DataRow("8d20kh2dl1cs>=5cf<2t3r4=2", "8d20kh2dl1cs>=5cf<2t3r4=2")]
+		public void DiceParseHandlesComplexOptionStrings(string input, string expected)
+		{
+			ParseTest(input, expected);
+		}
+
+		private void ParseTest(string input, string expected)
 		{
 			DiceExpression? expression = Dice.Parse(input);
 			Assert.IsNotNull(expression);
-			Tools.Write(input, expression);
-			Assert.IsTrue(expression.IsEmpty);
+			Assert.IsFalse(expression.IsEmpty);
+			string parsedExpression = expression.ToString();
+			DiceResult result = expression.Roll();
+			Tools.Write(input, expression, result, expected);
+			Assert.AreEqual(expected, parsedExpression);
 		}
 	}
 }
--- Tests/Tools.cs
+++ Tests/Tools.cs
@@ -2,8 +2,18 @@
 
 namespace cmdwtf.NumberStones.Tests
 {
+	/// <summary>
+	/// Miscellaneous tools for Tests
+	/// </summary>
 	public static class Tools
 	{
+		/// <summary>
+		/// A helper to write to the debug console from tests
+		/// </summary>
+		/// <param name="input">The input to the test</param>
+		/// <param name="parsed">The result of the parsed dice expression</param>
+		/// <param name="result">The result of the dice roll</param>
+		/// <param name="expected">What the test expected to generate</param>
 		public static void Write(string input, DiceExpression? parsed, DiceResult? result = null, object? expected = null)
 		{
 			Debug.WriteLine($"Input: {input}");
