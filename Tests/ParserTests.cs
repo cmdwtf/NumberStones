fd2a16375e5c809977859d4ecf317c1af90288df
Chris Dailey <nitz@users.noreply.github.com>
2021-05-13 10:13:37 -0400

    Add tests for labels and constants.

--- Tests/ParserTests.cs
+++ Tests/ParserTests.cs
@@ -54,7 +54,6 @@ namespace cmdwtf.NumberStones.Tests
 			);
 		}
 
-
 		[DataTestMethod]
 		[DataRow("1d6cx>14")]
 		[DataRow("1d6x")]
@@ -69,18 +68,35 @@ namespace cmdwtf.NumberStones.Tests
 			);
 		}
 
+		[DataTestMethod]
+		[DataRow("[CHA]5")]
+		[DataRow("1+[Guidance]2")]
+		[DataRow("[Guidance]")]
+		public void PoorlyPlacedLabelsFailToParse(string input)
+		{
+			Assert.ThrowsException<Exceptions.DiceExpressionParseException>(() =>
+				Dice.Parse(input)
+			);
+		}
+
+		[DataTestMethod]
+		[DataRow("5[CHA]", "5[CHA]")]
+		[DataRow("1+2[Guidance]", "1 + 2[Guidance]")]
+		[DataRow("1+2   [Guidance]", "1 + 2[Guidance]")] // spaces after constant shouldn't matter
+		public void ConstantsCanHaveLabels(string input, string expected) => ParseAndCheckExpected(input, expected);
+
 		[TestMethod]
-		public void DiceParseHandlesNoOptions() => ParseTest("1d20", "1d20");
+		public void DiceParseHandlesNoOptions() => ParseAndCheckExpected("1d20", "1d20");
 
 		[DataTestMethod]
 		[DataRow("8d6cf<5", "8d6cf<5")]
 		[DataRow("8d6cf<=5", "8d6cf<=5")]
-		public void DiceParseHandlesOptionCriticalFailure(string input, string expected) => ParseTest(input, expected);
+		public void DiceParseHandlesOptionCriticalFailure(string input, string expected) => ParseAndCheckExpected(input, expected);
 
 		[DataTestMethod]
 		[DataRow("8d6cs>5", "8d6cs>5")]
 		[DataRow("8d6cs>=5", "8d6cs>=5")]
-		public void DiceParseHandlesOptionCriticalSuccess(string input, string expected) => ParseTest(input, expected);
+		public void DiceParseHandlesOptionCriticalSuccess(string input, string expected) => ParseAndCheckExpected(input, expected);
 
 		[DataTestMethod]
 		[DataRow("8d6d", "8d6dl1")]
@@ -88,7 +104,7 @@ namespace cmdwtf.NumberStones.Tests
 		[DataRow("8d6dl2", "8d6dl2")]
 		[DataRow("8d6dh", "8d6dh1")]
 		[DataRow("8d6dh2", "8d6dh2")]
-		public void DiceParseHandlesOptionDrop(string input, string expected) => ParseTest(input, expected);
+		public void DiceParseHandlesOptionDrop(string input, string expected) => ParseAndCheckExpected(input, expected);
 
 		[DataTestMethod]
 		[DataRow("8d6!", "8d6!")]
@@ -97,7 +113,7 @@ namespace cmdwtf.NumberStones.Tests
 		[DataRow("8d6!>4", "8d6!>4")]
 		[DataRow("8d6!!>4", "8d6!!>4")]
 		[DataRow("8d6!p>4", "8d6!p>4")]
-		public void DiceParseHandlesOptionExplode(string input, string expected) => ParseTest(input, expected);
+		public void DiceParseHandlesOptionExplode(string input, string expected) => ParseAndCheckExpected(input, expected);
 
 		[DataTestMethod]
 		[DataRow("8d6k", "8d6kh1")]
@@ -105,26 +121,26 @@ namespace cmdwtf.NumberStones.Tests
 		[DataRow("8d6kh2", "8d6kh2")]
 		[DataRow("8d6kl", "8d6kl1")]
 		[DataRow("8d6kl2", "8d6kl2")]
-		public void DiceParseHandlesOptionKeep(string input, string expected) => ParseTest(input, expected);
+		public void DiceParseHandlesOptionKeep(string input, string expected) => ParseAndCheckExpected(input, expected);
 
 		[DataTestMethod]
 		[DataRow("8d6[cha]", "8d6[cha]")]
 		[DataRow("8d6[charisma is the only stat that matters, but dex is the only important one]", "8d6[charisma is the only stat that matters, but dex is the only important one]")]
-		public void DiceParseHandlesOptionLabel(string input, string expected) => ParseTest(input, expected);
+		public void DiceParseHandlesOptionLabel(string input, string expected) => ParseAndCheckExpected(input, expected);
 
 		[DataTestMethod]
 		[DataRow("8d6r2", "8d6r2")]
 		[DataRow("8d6r2r4", "8d6r2r4")]
-		public void DiceParseHandlesOptionReroll(string input, string expected) => ParseTest(input, expected);
+		public void DiceParseHandlesOptionReroll(string input, string expected) => ParseAndCheckExpected(input, expected);
 
 		[DataTestMethod]
 		[DataRow("8d6", "8d6")]
-		public void DiceParseHandlesOptionTarget(string input, string expected) => ParseTest(input, expected);
+		public void DiceParseHandlesOptionTarget(string input, string expected) => ParseAndCheckExpected(input, expected);
 
 		[DataTestMethod]
 		[DataRow("8d6t2", "8d6t2")]
 		[DataRow("8d6t2t4", "8d6t2t4")]
-		public void DiceParseHandlesOptionTwice(string input, string expected) => ParseTest(input, expected);
+		public void DiceParseHandlesOptionTwice(string input, string expected) => ParseAndCheckExpected(input, expected);
 
 		[DataTestMethod]
 		[DataRow("8d20", "8d20")]
@@ -144,9 +160,9 @@ namespace cmdwtf.NumberStones.Tests
 		[DataRow("8d20kh2dl1cs>=5cf<2t3r4=2[neat]!>19", "8d20kh2dl1cs>=5cf<2t3r4=2[neat]!>19")]
 		[DataRow("8d20kh2dl1cs>=5cf<2t3r4=2[neat]!!>19", "8d20kh2dl1cs>=5cf<2t3r4=2[neat]!!>19")]
 		[DataRow("8d20kh2dl1cs>=5cf<2t3r4=2[neat]!p>19", "8d20kh2dl1cs>=5cf<2t3r4=2[neat]!p>19")]
-		public void DiceParseHandlesComplexOptionStrings(string input, string expected) => ParseTest(input, expected);
+		public void DiceParseHandlesComplexOptionStrings(string input, string expected) => ParseAndCheckExpected(input, expected);
 
-		private static void ParseTest(string input, string expected)
+		private static void ParseAndCheckExpected(string input, string expected)
 		{
 			DiceExpression? expression = Dice.Parse(input);
 			Assert.IsNotNull(expression);
