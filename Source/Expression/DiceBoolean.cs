9b047b63de154a2cbb388879429c3cdeb0a1d29e
Chris Dailey <nitz@users.noreply.github.com>
2021-05-13 01:27:12 -0400

    Introduce DiceBoolean as a tristate bool of sorts.

---
+++ Source/Expression/DiceBoolean.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace cmdwtf.NumberStones.Expression
+{
+	/// <summary>
+	/// An enumeration in class form representing the three states a "boolean" value may be
+	/// in a dice result: True and False are self explanatory. Unset is used
+	/// as a default value to show that the status isn't a failure, but instead
+	/// wasn't checked at all.
+	///
+	/// Is this overkill? You betcha. But I like it a bit better than an enum,
+	/// and it's "safer" than a nullable bool.
+	/// </summary>
+	public class DiceBoolean
+	{
+		/// <summary>
+		/// The default value. Indicating the absense of true or false,
+		/// this value is used to show that the given <see cref="DiceBoolean"/>
+		/// wasn't evaluated at all.
+		/// </summary>
+		public static DiceBoolean Unset { get; } = new DiceBoolean(_unsetValue);
+		/// <summary>
+		/// The truthy value
+		/// </summary>
+		public static DiceBoolean True { get; } = new DiceBoolean(_trueValue);
+		/// <summary>
+		/// The falsy value
+		/// </summary>
+		public static DiceBoolean False { get; } = new DiceBoolean(_falseValue);
+
+		private const byte _unsetValue = 0;
+		private const byte _trueValue = 1;
+		private const byte _falseValue = 2;
+
+		private static SortedList<byte, DiceBoolean> Values { get; } = new();
+
+		private byte Value { get; init; }
+
+		private DiceBoolean(byte value = _unsetValue)
+		{
+			Value = value;
+		}
+
+		public static implicit operator DiceBoolean(bool value)
+		{
+			return value
+					   ? True
+					   : False;
+		}
+
+		public static implicit operator DiceBoolean(byte value)
+		{
+			return Values[value];
+		}
+
+		public static implicit operator byte(DiceBoolean value)
+		{
+			return value.Value;
+		}
+	}
+}
--- Source/Expression/DiceExpressionResult.cs
+++ Source/Expression/DiceExpressionResult.cs
@@ -2,22 +2,28 @@
 
 namespace cmdwtf.NumberStones.Expression
 {
+	/// <summary>
+	/// A record representing a dice expression result
+	/// </summary>
 	public record DiceExpressionResult : ExpressionResult
 	{
 		/// <summary>
 		/// A boolean representing if the value for the result is considered a success.
+		/// Unless modified by an option, the value will be <see cref="DiceBoolean.Unset"/>
 		/// </summary>
-		public bool Success { get; init; } = false;
+		public DiceBoolean Success { get; init; } = DiceBoolean.Unset;
 
 		/// <summary>
 		/// A boolean representing if the value for the result is considered a critical success.
+		/// Unless modified by an option, the value will be <see cref="DiceBoolean.Unset"/>
 		/// </summary>
-		public bool CriticalSuccess { get; init; } = false;
+		public DiceBoolean CriticalSuccess { get; init; } = DiceBoolean.Unset;
 
 		/// <summary>
 		/// A boolean representing if the value for the result is considered a critical failure.
+		/// Unless modified by an option, the value will be <see cref="DiceBoolean.Unset"/>
 		/// </summary>
-		public bool CriticalFailure { get; init; } = false;
+		public DiceBoolean CriticalFailure { get; init; } = DiceBoolean.Unset;
 
 		/// <summary>
 		/// The type of dice that was rolled to get this result.
