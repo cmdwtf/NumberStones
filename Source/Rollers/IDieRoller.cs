099aa1244a7a2acdb5f9047a068e3baefc791034
Chris Dailey <nitz@users.noreply.github.com>
2021-05-14 00:14:07 -0400

    Stubbing a test die roller.

--- Source/Rollers/IDieRoller.cs
+++ Source/Rollers/IDieRoller.cs
@@ -6,15 +6,15 @@
 	public interface IDieRoller
 	{
 		/// <summary>
+		/// The difference between the minimum and maximum value of this roller.
+		/// </summary>
+		long Range { get; }
+
+		/// <summary>
 		/// Rolls the die of specified sides.
 		/// </summary>
 		/// <param name="sides">The number of sides of the die to roll</param>
 		/// <returns>A number, perhaps random, based on the number of sides of the die</returns>
 		int RollDie(int sides);
-
-		/// <summary>
-		/// The difference between the minimum and maximum value of this roller.
-		/// </summary>
-		long Range { get; }
 	}
 }
---
+++ Tests/TestDieRoller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace cmdwtf.NumberStones.Tests
+{
+	public class TestDieRoller : Rollers.IDieRoller
+	{
+		public long Range { get; }
+
+		public int RollDie(int sides) => throw new NotImplementedException();
+	}
+}
